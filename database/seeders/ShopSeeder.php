9b4688725ba553345d926119b04ca03c61a0aa21
github Na0Se <minminnminm333@gmail.com>
2023-03-08 12:45:29 +0900

    first

---
+++ database/seeders/ShopSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ShopSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('shops')->insert([
+            ['name' => '高級食パン', 'area_id' => 1],
+            ['name' => '高級クロワッサン', 'area_id' =>2],
+        ]);
+    }
+}
